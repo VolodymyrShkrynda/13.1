305b472d4e3185a6fd92c81b2f603ce81ad3e077
VolodymyrShcrynda <volodymyr.shkrynda.itis.2020@lpnu.ua>
2021-01-14 11:57:55 +0200

    first commit

---
+++ Lab 13.1/Lab 13.1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <math.h>
+#include <iomanip>
+#include "dod.h"
+#include "sum.h"
+#include "var.h"
+using namespace std;
+using namespace nsDod;
+using namespace nsSum;
+using namespace nsVar;
+int main()
+{
+	cout << "xp = "; cin >> x_p;
+	cout << "xk = "; cin >> x_k;
+	cout << "dx = "; cin >> dx;
+	cout << "e = "; cin >> e;
+	cout << endl;
+	cout << fixed;
+	cout << "-----------------------------------------------------" << endl;
+	cout << "|" << setw(7) << setprecision(2) << "x" << " |"
+		<< setw(22) << setprecision(5) << "s" << " |"
+		<< setw(10) << setprecision(5) << "exp(x)" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+
+	cout << "-----------------------------------------------------" << endl;
+	x = x_p;
+	while (x <= x_k) {
+		nsSum::sum();
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(22) << setprecision(5) << s << " |"
+			<< setw(10) << setprecision(5) << exp(x) << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+		// тут слід використовувати форматний вивід
+		x += dx;
+	}
+	cout << "-----------------------------------------------------" << endl;
+	cin.get();
+	return 0;
+}
